33d07e870f6ce060cd268adac77326ed761101bf
Ravi Kothari <rsk328@gmail.com>
2019-02-28 23:08:10 -0500

    Fixed date bug and added full year stat feature

--- report.py
+++ report.py
@@ -1,15 +1,26 @@
-from datetime import datetime
+import datetime
+import time
 import boto3
 import pandas as pd
 from io import StringIO
+
 
 # Lambda function that will run each time each time a PUT request is done in the 'bvfco11' S3 bucket. This function
 # will calculate member statistics, including number of ambulance runs, number of engine runs, hours volunteered, etc.
 def my_lambda_handler(event, context):
     # S3 credentials
-    S3_BUCKET = 'bvfco11'
+    s3_bucket = 'bvfco11'
     s3 = boto3.client('s3')
     s3_resource = boto3.resource('s3')
+
+    now = time.localtime()
+
+    # Get previous month for naming the final CSV report
+    previous_month = (datetime.date(now.tm_year, now.tm_mon, 1) - datetime.timedelta(1)).strftime('%B')
+    previous_month_numerical = (datetime.date(now.tm_year, now.tm_mon, 1) - datetime.timedelta(1)).month
+
+    # Get current year
+    current_year = (datetime.date(now.tm_year, now.tm_mon, 1) - datetime.timedelta(1)).year
 
     # Prefixes for keys in the S3 bucket. This will ignore any date specific file names
     station_standby_prefix = 'Station_Standby_Record'
@@ -27,7 +38,7 @@ def my_lambda_handler(event, context):
 
     # Iterate through all objects in the S3 bucket, and set the appropriate key names to the variables
     # instantiated above
-    for obj in s3.list_objects(Bucket=S3_BUCKET)['Contents']:
+    for obj in s3.list_objects(Bucket=s3_bucket)['Contents']:
         key = obj['Key']
         if key.startswith(station_standby_prefix):
             station_standby_key = key
@@ -41,125 +52,231 @@ def my_lambda_handler(event, context):
             chief_report_key = key
 
     # Get station standby csv from S3 bucket
-    station_standby_obj = s3.get_object(Bucket=S3_BUCKET, Key=station_standby_key)
+    station_standby_obj = s3.get_object(Bucket=s3_bucket, Key=station_standby_key)
     station_standby_report = pd.read_csv(station_standby_obj['Body'], header=1)
 
     # Get member list csv from S3 bucket
-    member_list_obj = s3.get_object(Bucket=S3_BUCKET, Key=member_list_key)
+    member_list_obj = s3.get_object(Bucket=s3_bucket, Key=member_list_key)
     member_list = pd.read_csv(member_list_obj['Body'], header=2)
 
     # Get ambulance response report csv from S3 bucket
-    ambulance_report_obj = s3.get_object(Bucket=S3_BUCKET, Key=ambulance_report_key)
+    ambulance_report_obj = s3.get_object(Bucket=s3_bucket, Key=ambulance_report_key)
     ambulance_response_report = pd.read_csv(ambulance_report_obj['Body'], header=1)
 
     # Get engine response report csv from S3 bucket
-    engine_report_obj = s3.get_object(Bucket=S3_BUCKET, Key=engine_report_key)
+    engine_report_obj = s3.get_object(Bucket=s3_bucket, Key=engine_report_key)
     engine_response_report = pd.read_csv(engine_report_obj['Body'], header=1)
 
     # Get chief response report csv from S3 bucket
-    chief_report_obj = s3.get_object(Bucket=S3_BUCKET, Key=chief_report_key)
+    chief_report_obj = s3.get_object(Bucket=s3_bucket, Key=chief_report_key)
     chief_response_report = pd.read_csv(chief_report_obj['Body'], header=1)
 
     # Initialize the dictionaries to map members to their appropriate statistics
-    member_hours_dict = {}
-    member_incentive_due_dict = {}
-    member_ambulance_calls_dict = {}
-    member_engine_calls_dict = {}
-    member_chief_calls_dict = {}
+    member_hours_dict_for_month = {}
+    member_incentive_due_dict_for_month = {}
+    member_ambulance_calls_dict_for_month = {}
+    member_engine_calls_dict_for_month = {}
+    member_chief_calls_dict_for_month = {}
+
+    member_hours_dict_for_year = {}
+    member_incentive_due_dict_for_year = {}
+    member_ambulance_calls_dict_for_year = {}
+    member_engine_calls_dict_for_year = {}
+    member_chief_calls_dict_for_year = {}
 
     # Iterate through member list and initialize all members with 0 stats
     for index, row in member_list.iterrows():
-        if row.loc['Member'] not in member_hours_dict:
-            member_hours_dict[row.loc['Member']] = 0
-        if row.loc['Member'] not in member_incentive_due_dict:
-            member_incentive_due_dict[row.loc['Member']] = 0
-        if row.loc['Member'] not in member_ambulance_calls_dict:
-            member_ambulance_calls_dict[row.loc['Member']] = 0
-        if row.loc['Member'] not in member_engine_calls_dict:
-            member_engine_calls_dict[row.loc['Member']] = 0
-        if row.loc['Member'] not in member_chief_calls_dict:
-            member_chief_calls_dict[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_hours_dict_for_month:
+            member_hours_dict_for_month[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_incentive_due_dict_for_month:
+            member_incentive_due_dict_for_month[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_ambulance_calls_dict_for_month:
+            member_ambulance_calls_dict_for_month[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_engine_calls_dict_for_month:
+            member_engine_calls_dict_for_month[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_chief_calls_dict_for_month:
+            member_chief_calls_dict_for_month[row.loc['Member']] = 0
+
+        if row.loc['Member'] not in member_hours_dict_for_year:
+            member_hours_dict_for_year[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_incentive_due_dict_for_year:
+            member_incentive_due_dict_for_year[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_ambulance_calls_dict_for_year:
+            member_ambulance_calls_dict_for_year[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_engine_calls_dict_for_year:
+            member_engine_calls_dict_for_year[row.loc['Member']] = 0
+        if row.loc['Member'] not in member_chief_calls_dict_for_year:
+            member_chief_calls_dict_for_year[row.loc['Member']] = 0
 
     # Iterate through station standby report and calculate total hours volunteered by each member
     for index2, row in station_standby_report.iterrows():
-        member_hours_dict[row.loc['Submitted By']] += row.loc['Total Number of Hours']
+        date_in = row.loc['Date In']
+        month_of_standby = int(date_in.split('-')[1])
+
+        if month_of_standby == previous_month_numerical:
+            member_hours_dict_for_month[row.loc['Submitted By']] += row.loc['Total Number of Hours']
+            member_hours_dict_for_year[row.loc['Submitted By']] += row.loc['Total Number of Hours']
+        else:
+            member_hours_dict_for_year[row.loc['Submitted By']] += row.loc['Total Number of Hours']
 
     # Iterate through ambulance response report and calculate total ambulance incentive and
     # ambulance calls taken by each member
     for index3, row in ambulance_response_report.iterrows():
-        if row.loc['Transport'] == 'No':
-            member_incentive_due_dict[row.loc['Driver']] += 5
-            member_incentive_due_dict[row.loc['Aide/OIC']] += 5
-        elif row.loc['Transport'] == 'Yes':
-            member_incentive_due_dict[row.loc['Driver']] += 10
-            member_incentive_due_dict[row.loc['Aide/OIC']] += 10
-        else:
-            member_incentive_due_dict[row.loc['Driver']] += 5
-            member_incentive_due_dict[row.loc['Aide/OIC']] += 5
+        date_dispatched = row.loc['Date Dispatched']
+        month_of_call = int(date_dispatched.split('-')[1])
 
-        member_ambulance_calls_dict[row.loc['Driver']] += 1
-        member_ambulance_calls_dict[row.loc['Aide/OIC']] += 1
-        if not (pd.isnull(row.loc['3rd'])):
-            member_ambulance_calls_dict[row.loc['3rd']] += 1
-        if not (pd.isnull(row.loc['Additional Crew'])):
-            member_ambulance_calls_dict[row.loc['Additional Crew']] += 1
+        if month_of_call == previous_month_numerical:
+            if row.loc['Transport'] == 'No':
+                member_incentive_due_dict_for_month[row.loc['Driver']] += 5
+                member_incentive_due_dict_for_month[row.loc['Aide/OIC']] += 5
+                member_incentive_due_dict_for_year[row.loc['Driver']] += 5
+                member_incentive_due_dict_for_year[row.loc['Aide/OIC']] += 5
+            elif row.loc['Transport'] == 'Yes':
+                member_incentive_due_dict_for_month[row.loc['Driver']] += 10
+                member_incentive_due_dict_for_month[row.loc['Aide/OIC']] += 10
+                member_incentive_due_dict_for_year[row.loc['Driver']] += 10
+                member_incentive_due_dict_for_year[row.loc['Aide/OIC']] += 10
+            else:
+                member_incentive_due_dict_for_month[row.loc['Driver']] += 5
+                member_incentive_due_dict_for_month[row.loc['Aide/OIC']] += 5
+                member_incentive_due_dict_for_year[row.loc['Driver']] += 5
+                member_incentive_due_dict_for_year[row.loc['Aide/OIC']] += 5
+
+            member_ambulance_calls_dict_for_month[row.loc['Driver']] += 1
+            member_ambulance_calls_dict_for_month[row.loc['Aide/OIC']] += 1
+            member_ambulance_calls_dict_for_year[row.loc['Driver']] += 1
+            member_ambulance_calls_dict_for_year[row.loc['Aide/OIC']] += 1
+            if not (pd.isnull(row.loc['3rd'])):
+                member_ambulance_calls_dict_for_month[row.loc['3rd']] += 1
+                member_ambulance_calls_dict_for_year[row.loc['3rd']] += 1
+            if not (pd.isnull(row.loc['Additional Crew'])):
+                member_ambulance_calls_dict_for_month[row.loc['Additional Crew']] += 1
+                member_ambulance_calls_dict_for_year[row.loc['Additional Crew']] += 1
+        else:
+            if row.loc['Transport'] == 'No':
+                member_incentive_due_dict_for_year[row.loc['Driver']] += 5
+                member_incentive_due_dict_for_year[row.loc['Aide/OIC']] += 5
+            elif row.loc['Transport'] == 'Yes':
+                member_incentive_due_dict_for_year[row.loc['Driver']] += 10
+                member_incentive_due_dict_for_year[row.loc['Aide/OIC']] += 10
+            else:
+                member_incentive_due_dict_for_year[row.loc['Driver']] += 5
+                member_incentive_due_dict_for_year[row.loc['Aide/OIC']] += 5
+
+            member_ambulance_calls_dict_for_year[row.loc['Driver']] += 1
+            member_ambulance_calls_dict_for_year[row.loc['Aide/OIC']] += 1
+            if not (pd.isnull(row.loc['3rd'])):
+                member_ambulance_calls_dict_for_year[row.loc['3rd']] += 1
+            if not (pd.isnull(row.loc['Additional Crew'])):
+                member_ambulance_calls_dict_for_year[row.loc['Additional Crew']] += 1
 
     # Iterate through engine response report and calculate total engine incentive and engine calls taken by each member
     for index4, row in engine_response_report.iterrows():
-        member_incentive_due_dict[row.loc['Driver']] += 5
-        member_incentive_due_dict[row.loc['Officer-In-Charge']] += 5
+        date_dispatched = row.loc['Date Dispatched']
+        month_of_call = int(date_dispatched.split('-')[1])
 
-        member_engine_calls_dict[row.loc['Driver']] += 1
-        member_engine_calls_dict[row.loc['Officer-In-Charge']] += 1
-        if not (pd.isnull(row.loc['Line'])):
-            member_engine_calls_dict[row.loc['Line']] += 1
-        if not (pd.isnull(row.loc['Backup'])):
-            member_engine_calls_dict[row.loc['Backup']] += 1
-        if not (pd.isnull(row.loc['Bars'])):
-            member_engine_calls_dict[row.loc['Bars']] += 1
-        if not (pd.isnull(row.loc['Layout'])):
-            member_engine_calls_dict[row.loc['Layout']] += 1
-        if not (pd.isnull(row.loc['Observer'])):
-            member_engine_calls_dict[row.loc['Observer']] += 1
+        if month_of_call == previous_month_numerical:
+            member_incentive_due_dict_for_month[row.loc['Driver']] += 5
+            member_incentive_due_dict_for_month[row.loc['Officer-In-Charge']] += 5
+            member_incentive_due_dict_for_year[row.loc['Driver']] += 5
+            member_incentive_due_dict_for_year[row.loc['Officer-In-Charge']] += 5
+
+            member_engine_calls_dict_for_month[row.loc['Driver']] += 1
+            member_engine_calls_dict_for_month[row.loc['Officer-In-Charge']] += 1
+            member_engine_calls_dict_for_year[row.loc['Driver']] += 1
+            member_engine_calls_dict_for_year[row.loc['Officer-In-Charge']] += 1
+            if not (pd.isnull(row.loc['Line'])):
+                member_engine_calls_dict_for_month[row.loc['Line']] += 1
+                member_engine_calls_dict_for_year[row.loc['Line']] += 1
+            if not (pd.isnull(row.loc['Backup'])):
+                member_engine_calls_dict_for_month[row.loc['Backup']] += 1
+                member_engine_calls_dict_for_year[row.loc['Backup']] += 1
+            if not (pd.isnull(row.loc['Bars'])):
+                member_engine_calls_dict_for_month[row.loc['Bars']] += 1
+                member_engine_calls_dict_for_year[row.loc['Bars']] += 1
+            if not (pd.isnull(row.loc['Layout'])):
+                member_engine_calls_dict_for_month[row.loc['Layout']] += 1
+                member_engine_calls_dict_for_year[row.loc['Layout']] += 1
+            if not (pd.isnull(row.loc['Observer'])):
+                member_engine_calls_dict_for_month[row.loc['Observer']] += 1
+                member_engine_calls_dict_for_year[row.loc['Observer']] += 1
+        else:
+            member_incentive_due_dict_for_year[row.loc['Driver']] += 5
+            member_incentive_due_dict_for_year[row.loc['Officer-In-Charge']] += 5
+
+            member_engine_calls_dict_for_year[row.loc['Driver']] += 1
+            member_engine_calls_dict_for_year[row.loc['Officer-In-Charge']] += 1
+            if not (pd.isnull(row.loc['Line'])):
+                member_engine_calls_dict_for_year[row.loc['Line']] += 1
+            if not (pd.isnull(row.loc['Backup'])):
+                member_engine_calls_dict_for_year[row.loc['Backup']] += 1
+            if not (pd.isnull(row.loc['Bars'])):
+                member_engine_calls_dict_for_year[row.loc['Bars']] += 1
+            if not (pd.isnull(row.loc['Layout'])):
+                member_engine_calls_dict_for_year[row.loc['Layout']] += 1
+            if not (pd.isnull(row.loc['Observer'])):
+                member_engine_calls_dict_for_year[row.loc['Observer']] += 1
 
     # Iterate through chief response report and calculate total chief calls taken by each member
     for index5, row in chief_response_report.iterrows():
-        member_chief_calls_dict[row.loc['Chief']] += 1
-        if not (pd.isnull(row.loc['Aide'])):
-            member_chief_calls_dict[row.loc['Aide']] += 1
+        date_dispatched = row.loc['Date Dispatched']
+        month_of_call = int(date_dispatched.split('-')[1])
+
+        if month_of_call == previous_month_numerical:
+            member_chief_calls_dict_for_month[row.loc['Chief']] += 1
+            member_chief_calls_dict_for_year[row.loc['Chief']] += 1
+            if not (pd.isnull(row.loc['Aide'])):
+                member_chief_calls_dict_for_month[row.loc['Aide']] += 1
+                member_chief_calls_dict_for_year[row.loc['Aide']] += 1
+        else:
+            member_chief_calls_dict_for_year[row.loc['Chief']] += 1
+            if not (pd.isnull(row.loc['Aide'])):
+                member_chief_calls_dict_for_year[row.loc['Aide']] += 1
 
     # Initialize member lists and stats for conversion into Python DataFrame
     member_names = []
-    member_hours = []
-    member_incentive = []
-    member_ambulance_calls = []
-    member_engine_calls = []
-    member_chief_calls = []
+    member_hours_for_month = []
+    member_incentive_for_month = []
+    member_ambulance_calls_for_month = []
+    member_engine_calls_for_month = []
+    member_chief_calls_for_month = []
+    member_hours_for_year = []
+    member_incentive_for_year = []
+    member_ambulance_calls_for_year = []
+    member_engine_calls_for_year = []
+    member_chief_calls_for_year = []
 
     # Add all members and their associated stats into the appropriate list
-    for member in member_hours_dict:
+    for member in member_hours_dict_for_month:
         member_names.append(member)
-        member_hours.append(member_hours_dict[member])
-        member_incentive.append(member_incentive_due_dict[member])
-        member_ambulance_calls.append(member_ambulance_calls_dict[member])
-        member_engine_calls.append(member_engine_calls_dict[member])
-        member_chief_calls.append(member_chief_calls_dict[member])
+        member_hours_for_month.append(member_hours_dict_for_month[member])
+        member_incentive_for_month.append(member_incentive_due_dict_for_month[member])
+        member_ambulance_calls_for_month.append(member_ambulance_calls_dict_for_month[member])
+        member_engine_calls_for_month.append(member_engine_calls_dict_for_month[member])
+        member_chief_calls_for_month.append(member_chief_calls_dict_for_month[member])
+        member_hours_for_year.append(member_hours_dict_for_year[member])
+        member_incentive_for_year.append(member_incentive_due_dict_for_year[member])
+        member_ambulance_calls_for_year.append(member_ambulance_calls_dict_for_year[member])
+        member_engine_calls_for_year.append(member_engine_calls_dict_for_year[member])
+        member_chief_calls_for_year.append(member_chief_calls_dict_for_year[member])
 
     # Set dictionary for final CSV report labels
     member_hour_final_dict = {
         'Member': member_names,
-        'Station Standby Hours Reported': member_hours,
-        'Incentive Due': member_incentive,
-        'Ambulance Calls Taken': member_ambulance_calls,
-        'Engine Calls Taken': member_engine_calls,
-        'Chief Calls Taken': member_chief_calls
+        'Station Standby Hours Reported in ' + str(previous_month): member_hours_for_month,
+        'Incentive Due in ' + str(previous_month): member_incentive_for_month,
+        'Ambulance Calls Taken in ' + str(previous_month): member_ambulance_calls_for_month,
+        'Engine Calls Taken in ' + str(previous_month): member_engine_calls_for_month,
+        'Chief Calls Taken in ' + str(previous_month): member_chief_calls_for_month,
+        'Station Standby Hours Reported in ' + str(current_year): member_hours_for_year,
+        'Incentive Due in ' + str(current_year): member_incentive_for_year,
+        'Ambulance Calls Taken in ' + str(current_year): member_ambulance_calls_for_year,
+        'Engine Calls Taken in ' + str(current_year): member_engine_calls_for_year,
+        'Chief Calls Taken in ' + str(current_year): member_chief_calls_for_year
     }
 
-    # Get current month for naming the final CSV report
-    currentMonth = datetime.now().strftime('%B')
-
     # Set the report title and initialize a buffer for conversion into a CSV
-    report_upload = str(currentMonth) + '_Station_11_Report.csv'
+    report_upload = str(previous_month) + '_Station_11_Report.csv'
     csv_buffer = StringIO()
 
     # Convert the data from lists into a Python DataFrame
@@ -169,7 +286,7 @@ def my_lambda_handler(event, context):
     member_hours_dataframe.to_csv(csv_buffer, index=False)
 
     # Put request for final CSV report into S3 bucket
-    s3_resource.Object(S3_BUCKET, report_upload).put(Body=csv_buffer.getvalue())
+    s3_resource.Object(s3_bucket, report_upload).put(Body=csv_buffer.getvalue())
 
     # Return success
     return 0
